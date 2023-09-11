719600387c754a1261eeb2e12f82b6291aa9727f
Tran Duy Anh <anh.td.jv@gmail.com>
2023-09-11 11:16:29 +0700

    Fix registration and login

--- air_quality_management/myapp/views.py
+++ air_quality_management/myapp/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import AirQualityData, Location, Pollutant, Sensor
-from django.shortcuts import render
-
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.http import HttpResponseRedirect
 from django.views import View
+from .forms import LoginForm, RegisterForm
+
 
 def homepage(request):
     """
@@ -17,31 +20,63 @@ def homepage(request):
     }
     return render(request, 'homepage.html', context)
 
-def login_view(request):
-    """
-    View for the login.
-    """
-    # You can add logic here to fetch data from your models if needed
-    # For now, we'll render the homepage template with some dummy data
-    dummy_data = []
 
-    context = {
-        'data': dummy_data,
-    }
-    return render(request, 'login.html', context)
+def login_view(request):
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request, 'registration/login.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, f'Welcome back!')
+                return HttpResponseRedirect('user')
+
+        messages.error(request, f'Invalid username or password')
+        return render(request, 'registration/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, f"You've been logged out.")
+    return HttpResponseRedirect('login')
+
 
 def register_view(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'registration/register.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have signed up successfully')
+            login(request, user)
+            return HttpResponseRedirect('user')
+        else:
+            return render(request, 'registration/register.html', {'form': form})
+
+
+def user_view(request):
     """
-    View for the login.
+    View to display a list of air quality data.
     """
-    # You can add logic here to fetch data from your models if needed
-    # For now, we'll render the homepage template with some dummy data
     dummy_data = []
 
     context = {
         'data': dummy_data,
     }
-    return render(request, 'register.html', context)
+    return render(request, 'user/user_profile.html', context)
+
 
 class city_details(View):
     template_name = 'city_details.html'
@@ -56,6 +91,7 @@ class city_details(View):
 
         return render(request, self.template_name, context)
 
+
 def air_quality_data_list(request):
     """
     View to display a list of air quality data.
@@ -65,6 +101,7 @@ def air_quality_data_list(request):
         'air_quality_data': air_quality_data,
     }
     return render(request, 'data/air_quality_data_list.html', context)
+
 
 def air_quality_data_detail(request, data_id):
     """
