7cddf19daa83478e095a978cbb546fe1711ce6db
Tran Duy Anh <anh.td.jv@gmail.com>
2023-09-19 09:50:09 +0700

    Report

--- air_quality_management/myapp/urls.py
+++ air_quality_management/myapp/urls.py
@@ -7,7 +7,7 @@ from .views import ChangePasswordView
 urlpatterns = [
     path('', views.index, name='index'),
     path('homepage/', views.homepage, name='homepage'),
-    path('city_details/<int:location_id>/', views.city_details.as_view(), name='city_details'),
+    path('city_details/<int:pollutant_id>/', views.city_details.as_view(), name='city_details'),
     path('air-quality-data/', views.air_quality_data_list, name='air_quality_data_list'),
     path('air-quality-data/<uuid:data_id>/', views.air_quality_data_detail, name='air_quality_data_detail'),
     path('login/', views.login_view, name='login'),
@@ -18,4 +18,5 @@ urlpatterns = [
     path('edit-profile/', edit_profile, name='edit-profile'),
     path('password-change/', ChangePasswordView.as_view(), name='password_change'),
     path('reports-history/', views.reports_history, name='reports_history'),
+    path('report_air_quality', views.report_air_quality, name='report_air_quality'),
 ]
--- air_quality_management/myapp/views.py
+++ air_quality_management/myapp/views.py
@@ -1,8 +1,7 @@
+from datetime import datetime
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import AirQualityData, Location, Pollutant, Sensor
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .forms import UpdateUserForm, UpdateProfileForm
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
@@ -11,7 +10,11 @@ from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.db import transaction
+from .models import AirQualityData, Pollutant, Sensor
 from .forms import LoginForm, RegisterForm
+from .forms import UpdateUserForm, UpdateProfileForm
+from .forms import AirQualityForm_User
 
 
 def index(request):
@@ -26,6 +29,7 @@ def index(request):
         'data': dummy_data,
     }
     return render(request, 'index.html', context)
+
 
 def homepage(request):
     """
@@ -94,12 +98,12 @@ def user_view(request):
 class city_details(View):
     template_name = 'city_details.html'
 
-    def get(self, request, location_id, *args, **kwargs):
+    def get(self, request, pollutant_id, *args, **kwargs):
         # Retrieve the AirQualityData object based on the location_id
-        air_quality_data = get_object_or_404(AirQualityData, location_id_id=location_id)
+        air_quality_data = get_object_or_404(AirQualityData, pollutant_id_id=pollutant_id)
 
         # Retrieve the pollutant data related to the AirQualityData object
-        pollutants = get_object_or_404(Pollutant,  id=location_id)
+        pollutants = get_object_or_404(Pollutant, id=pollutant_id)
 
         context = {
             'air_quality_data': air_quality_data,
@@ -164,6 +168,7 @@ class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
     success_message = _('Successfully Changed Your Password')
     success_url = reverse_lazy('users-profile')
 
+
 @login_required
 def reports_history(request):
     air_quality_data = AirQualityData.objects.filter(provider=request.user.username)
@@ -172,3 +177,66 @@ def reports_history(request):
         'air_quality_data': air_quality_data,
     }
     return render(request, 'reports_history.html', context)
+
+
+def _pollution_level(air_quality_index):
+    if air_quality_index <= 33:
+        return _("Very Good")
+    elif 34 <= air_quality_index <= 66:
+        return _("Good")
+    elif 67 <= air_quality_index <= 99:
+        return _("Fair")
+    elif 100 <= air_quality_index <= 149:
+        return _("Bad")
+    elif 150 <= air_quality_index <= 200:
+        return _("Very Bad")
+    elif air_quality_index > 200:
+        return _("Hazardous")
+
+
+@login_required
+def report_air_quality(request):
+    if request.method == 'POST':
+        form = AirQualityForm_User(request.POST)
+        if form.is_valid():
+            city = form.cleaned_data['location']
+            latitude = form.cleaned_data['latitude']
+            longitude = form.cleaned_data['longitude']
+            so2 = form.cleaned_data['so2']
+            o3 = form.cleaned_data['o3']
+            pm2_5 = form.cleaned_data['pm2_5']
+            pm10 = form.cleaned_data['pm10']
+
+            with transaction.atomic():
+                # Save pollutant to database
+                pollutant_obj = Pollutant(SO2=so2, O3=o3, PM2_5=pm2_5, PM10=pm10)
+                pollutant_obj.save()
+
+                # Get primary key
+                pol_id = pollutant_obj
+
+                # Pollution level calculation
+                SO2STANDARD = 0.25
+                O3STANDARD = 0.065
+                PM2_5STANDARD = 25
+                PM10STANDARD = 50
+
+                so2_aqi = so2 * 100 / SO2STANDARD  # Input for 8h period - Standard 0.25pmm
+                o3_aqi = o3 * 100 / O3STANDARD  # Input for 8h period - Standard 0.065pmm
+                pm2_5_aqi = pm2_5 * 100 / PM2_5STANDARD  # Input for 24h period - Standard 25ug/m3
+                pm10_aqi = pm10 * 100 / PM10STANDARD  # Input for 24h period - Standard 50ug/m3
+                main_pol = {so2_aqi: "SO2", o3_aqi: "O3", pm2_5_aqi: "PM2.5", pm10_aqi: "PM10"}
+                air_quality_index = max(so2_aqi, o3_aqi, pm2_5_aqi, pm10_aqi)
+
+                # Get username
+                aqi_data = AirQualityData(city=city, latitude=latitude, longitude=longitude,
+                                          pollutant_id=pol_id, main_pollutant=main_pol.get(max(main_pol)),
+                                          air_quality_index=air_quality_index,
+                                          timestamp=datetime.now(), pol_level=_pollution_level(air_quality_index),
+                                          provider=request.user.username)
+                aqi_data.save()
+                return redirect('homepage')
+    else:
+        form = AirQualityForm_User()
+
+    return render(request, 'user/user_aqi_form_create.html', {'form': form})
