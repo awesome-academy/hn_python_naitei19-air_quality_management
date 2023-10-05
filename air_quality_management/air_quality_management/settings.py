5e39e05d78e48749d1885a0e5219fbaca610d944
Quan Dang <danganhquan246@gmail.com>
2023-10-05 10:08:23 +0700

    Unit test for a model's fields

--- air_quality_management/air_quality_management/settings.py
+++ air_quality_management/air_quality_management/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'myapp.apps.MyappConfig',
     'leaflet',
     'sweetify',
+    'myapp_tests.apps.MyappTestsConfig',
 ]
 
 MIDDLEWARE = [
---
+++ air_quality_management/myapp_tests/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyappTestsConfig(AppConfig):
+    name = 'myapp_tests'
